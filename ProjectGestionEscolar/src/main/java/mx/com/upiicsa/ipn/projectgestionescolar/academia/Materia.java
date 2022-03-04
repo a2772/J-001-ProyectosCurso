68d7591ec7f76d6785447dc14a0d359308a24afd
IvanGV08 <ivangarv73@gmail.com>
2022-03-03 18:14:58 -0600

    test02

--- ProjectGestionEscolar/src/main/java/mx/com/upiicsa/ipn/projectgestionescolar/academia/Materia.java
+++ ProjectGestionEscolar/src/main/java/mx/com/upiicsa/ipn/projectgestionescolar/academia/Materia.java
@@ -1,5 +1,33 @@
 package mx.com.upiicsa.ipn.projectgestionescolar.academia;
 
 public class Materia {
+    private String materia;
+    private int horas;
+    private float creditosParaInscribir;
+
+    public String getMateria() {
+        return materia;
+    }
+
+    public void setMateria(String materia) {
+        this.materia = materia;
+    }
+
+    public int getHoras() {
+        return horas;
+    }
+
+    public void setHoras(int horas) {
+        this.horas = horas;
+    }
+
+    public float getCreditosParaInscribir() {
+        return creditosParaInscribir;
+    }
+
+    public void setCreditosParaInscribir(float creditosParaInscribir) {
+        this.creditosParaInscribir = creditosParaInscribir;
+    }
+    
     
 }
