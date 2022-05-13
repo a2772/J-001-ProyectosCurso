187c8f23007814d529a21fc96c47c7632e890eee
paris <paris.140815@gmail.com>
2022-05-13 13:56:07 -0500

    Calculadora inicio

---
+++ Calculadora/src/main/java/mx/ipn/upiicsa/calculadora/calculator/logic/LogicCalculator.java
@@ -0,0 +1,9 @@
+package mx.ipn.upiicsa.calculadora.calculator.logic;
+public class LogicCalculator {
+    //Constantes
+    
+    //Variables
+    private double argumentOne;
+    private double argumentTwo;
+    private int operator;
+}
